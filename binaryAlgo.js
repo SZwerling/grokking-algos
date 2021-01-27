7b0ebbb10126010a96034e4bf1d6baf8d0a61ee8
Slim <sazwerling@gmail.com>
2021-01-26 17:31:55 -0700

    binary search

---
+++ binaryAlgo.js
@@ -0,0 +1,33 @@
+//start index
+//middle index
+//end index
+//if target greater, move start index to middle index
+//if target is smaller, move end index to middle index
+
+const binarySearch = (array, target) => {
+    let startIndex = 0;
+    let endIndex = array.length - 1;
+    while(startIndex <= endIndex) {
+      let middleIndex = Math.floor((startIndex + endIndex) / 2);
+      if(target === array[middleIndex]) {
+        return console.log("Target was found at index " + middleIndex);
+      }
+      if(target > array[middleIndex]) {
+        console.log("Searching the right side of Array")
+        startIndex = middleIndex + 1;
+      }
+      if(target < array[middleIndex]) {
+        console.log("Searching the left side of array")
+        endIndex = middleIndex - 1;
+      }
+      else {
+        console.log("Not Found this loop iteration. Looping another iteration.")
+      }
+    }
+    
+    console.log("Target value not found in array");
+  }
+
+  arrt = [1, 2, 3, 4, 7, 9, 10, 14, 16, 22, 34]
+  binarySearch(arrt, 34)
+
