1479f2f41d47f597c4ddf27fdf3657367d1a6671
Slim <sazwerling@gmail.com>
2021-01-26 21:05:13 -0700

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,27 @@
+//selection sort without making a separate array
+//  O(n*n)  or big O(n squared)
+
+function selectionSort(inputArr) { 
+    let n = inputArr.length;
+        
+    for(let i = 0; i < n; i++) {
+        // Finding the smallest number
+        let min = i;
+        for(let j = i+1; j < n; j++){
+            if(inputArr[j] < inputArr[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             // making a swap, i is at 0 the first time here
+             let tmp = inputArr[i]; 
+             inputArr[i] = inputArr[min];
+             inputArr[min] = tmp;      
+        }
+    }
+    return inputArr;
+}
+
+arr1 = [5, 3, 1, 9, 6, 8, 5]
+selectionSort(arr1)
+console.log(arr1)
