61a67ab6df931ec085c1cca8a23ec8da442f5720
unknown <sazwerling@gmail.com>
2022-01-06 22:39:27 -0600

    other selection sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,29 +1,48 @@
 //selection sort without making a separate array
 //  O(n*n)  or big O(n squared)
 
-function selectionSort(inputArr) { 
-    let n = inputArr.length;
-        
-    for(let i = 0; i < n; i++) {
-        // Finding the smallest number
-        let min = i;
-        
-        for(let j = i+1; j < n; j++){
-            if(inputArr[j] < inputArr[min]) {
-                min=j; 
-            }
+function selectionSort(inputArr) {
+   let n = inputArr.length;
+
+   for (let i = 0; i < n; i++) {
+      // Finding the smallest number
+      let min = i;
+
+      for (let j = i + 1; j < n; j++) {
+         if (inputArr[j] < inputArr[min]) {
+            min = j;
          }
-         if (min !== i) {
-             //console.log(min)
-             // making a swap, i is at 0 the first time here
-             let tmp = inputArr[i]; 
-             inputArr[i] = inputArr[min];
-             inputArr[min] = tmp;      
-        }
-    }
-    return inputArr;
+      }
+      if (min !== i) {
+         //console.log(min)
+         // making a swap, i is at 0 the first time here
+         let tmp = inputArr[i];
+         inputArr[i] = inputArr[min];
+         inputArr[min] = tmp;
+      }
+   }
+   return inputArr;
 }
 
-arr1 = [5, 3, 0, 2]
-selectionSort(arr1)
-console.log(arr1)
+arr1 = [5, 3, 0, 2];
+selectionSort(arr1);
+console.log(arr1);
+
+function selectionSort2(arr) {
+   for (let i = 0; i < arr.length; i++) {
+      let indexOfMin = i;
+      for (let j = i + 1; j < arr.length; j++) {
+         if (arr[j] < arr[indexOfMin]) {
+            indexOfMin = j;
+         }
+      }
+      if (indexOfMin != i) {
+         let lesser = arr[indexOfMin];
+         arr[indexOfMin] = arr[i];
+         arr[i] = lesser;
+      }
+   }
+   return arr;
+}
+
+console.log(selectionSort2(arr1));
