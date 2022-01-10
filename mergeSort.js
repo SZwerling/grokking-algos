8e024064b996fcd028b2d877f48b1dde59170f83
unknown <sazwerling@gmail.com>
2022-01-09 21:11:36 -0600

    merge and mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,21 @@
+
+const arr1 = [0, 50];
+const arr2 = [-1, 52, 60];
+const arr3 = [1, 2, 3, 4, 0, 50, 9, -1, 52, 60, 8, 7, 6, 5]
+
+function mergeSort(arr){
+    if(arr.length === 1){
+        return arr
+    }
+
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);  //up to but not including center
+    const right = arr.slice(center)
+    return merge(mergeSort(left), mergeSort(right));  //recursive function to mergeSort that splits so then only
+}                                                     //two elements at a time sent into merge
+console.log(mergeSort(arr3))
+
+
 //
 function merge(left, right){
     let results = [];
@@ -11,7 +29,4 @@ function merge(left, right){
     return [...results, ...left, ...right]
 }
 
-const arr1 = [0, 50];
-const arr2 = [-1, 52, 60];
 
-console.log(merge(arr1, arr2))
