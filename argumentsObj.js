2abae54c32e86d4f2cd9691cb287c8a54d6598f9
unknown <sazwerling@gmail.com>
2022-01-15 11:35:15 -0600

    symmetric difference

--- argumentsObj.js
+++ argumentsObj.js
@@ -2,20 +2,38 @@
 // You can refer to a function's arguments inside that function by using its arguments object. 
 // It has entries for each argument the function was called with, with the first entry's index at 0.
 //can only use .length
-
-
-const a = [1, 2, 3]
-const b = [4, 5, 6]
-const c = [7, 8, 9]
-const mam = function(){
-    let args = [...arguments]
-    console.log(args)
-}
-
-console.log(mam(a, b, c))
-
 //However, it can be converted to a real Array:
     //let args = [...arguments];
     //let args = Array.prototype.slice.call(arguments);
     //let args = Array.from(arguments);
     //var args = [].slice.call(arguments);
+
+
+const a = [1, 2, 3]
+const b = [3, 5, 6]
+const c = [7, 6, 9]
+const sym = function(){
+    let args = [...arguments];
+
+    const symDiff = function(arr1, arr2){
+        let result = []
+        arr1.forEach((item) => {
+            if(arr1.indexOf(item) < 0 && arr2.indexOf(item) < 0) {
+                result.push(item)
+            }
+        });
+
+        arr2.forEach((item) => {
+            if(arr2.indexOf(item) < 0 && arr1.indexOf(item) < 0){
+                result.push(item)
+            }
+        });
+        return result
+    }
+
+    return args.reduce(symDiff)
+    
+}
+
+console.log(sym(a, b, c))
+
--- symmetricDifference.js
+++ symmetricDifference.js
@@ -6,13 +6,90 @@
 // you must complete one operation at a time. 
 // Thus, for sets A and B above, and C = {2, 3}, A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
 
+//using arguments object.
 
-function sym(args) {
+
+const a = [1, 2, 3]
+const b = [4, 5, 1]
+
+const sym1 = function(){
+    const args = [...arguments];
+
+    const symDiff = function(arr1, arr2){
+        let result = [];
+        arr1.forEach((item) => {
+            if(result.indexOf(item) < 0 && arr2.indexOf(item) < 0) {
+                result.push(item)
+            }
+        });
+
+        arr2.forEach((item) => {
+            if(result.indexOf(item) < 0 && arr1.indexOf(item) < 0){
+                result.push(item)
+            }
+        });
+        return result
+    }
+
+    return args.reduce(symDiff)
     
-    for(let el of args[0])
-    if(arg[1])
-    
-    return args;
+}
+
+function sym2() {
+  var args = [];
+  for (var i = 0; i < arguments.length; i++) {
+    args.push(arguments[i]);
   }
-  
-  sym([1, 2, 3], [5, 2, 1, 4]);
+
+  function symDiff(arrayOne, arrayTwo) {
+    var result = [];
+
+    arrayOne.forEach(function(item) {
+      if (arrayTwo.indexOf(item) < 0 && result.indexOf(item) < 0) {
+        result.push(item);
+      }
+    });
+
+    arrayTwo.forEach(function(item) {
+      if (arrayOne.indexOf(item) < 0 && result.indexOf(item) < 0) {
+        result.push(item);
+      }
+    });
+
+    return result;
+  }
+
+  // Apply reduce method to args array, using the symDiff function
+  return args.reduce(symDiff);
+}
+
+function sym3() {
+  // Convert the argument object into a proper array
+  var args = Array.prototype.slice.call(arguments);
+
+  // Return the symmetric difference of 2 arrays
+  var getDiff = function(arr1, arr2) {
+    // Returns items in arr1 that don't exist in arr2
+    function filterFunction(arr1, arr2) {
+      return arr1.filter(function(item) {
+        return arr2.indexOf(item) === -1;
+      });
+    }
+
+    // Run filter function on each array against the other
+    return filterFunction(arr1, arr2).concat(filterFunction(arr2, arr1));
+  };
+
+  // Reduce all arguments getting the difference of them
+  var summary = args.reduce(getDiff, []);
+
+  // Run filter function to get the unique values
+  var unique = summary.filter(function(elem, index, self) {
+    return index === self.indexOf(elem);
+  });
+  return unique;
+}
+
+
+
+console.log(sym1([1, 2, 5], [2, 3, 5], [3, 4, 5]))
