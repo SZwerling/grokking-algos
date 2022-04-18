daf1d4ec95b729cdef1f9372824ed10fb5391d4d
unknown <sazwerling@gmail.com>
2022-04-18 09:53:06 -0500

    freq counter or multiple pointers for areDuplicates

--- areDuplicates.js
+++ areDuplicates.js
@@ -1,9 +1,11 @@
 //function that checks variable number of arguments for duplicates
 //areThereDuplicates(1, 2, 3) //false
 //areThereDuplicates('a', 'a', 'b', 'c') //true
+//arguments is an array-like object available within all non-arrow functions
+//...args is the rest parameter. Allows a function to take an indefinite number of arguments as an array.
 
 function areThereDuplicates() {
-    
+    console.log(arguments)
     let freq = {}
     for(let val in arguments){ 
         freq[arguments[val]] = (freq[arguments[val]] || 0) + 1
@@ -25,7 +27,7 @@ function areThereDuplicates2(...args){
     args.sort((a,b) => a > b);
     let start = 0
     let next = 1
-    console.log(args)
+    
     while(next < args.length){
         if(args[start] === args[next]){
             return true
@@ -43,5 +45,20 @@ function areThereDuplicates3() {
   return new Set(arguments).size !== arguments.length; 
 }
 //The size accessor property returns the number of (unique) elements in a Set object.
+
+function areThereDuplicates4(...args) {
+    console.log(args)
+    let freq = {}
+    for(let val of args){ 
+        freq[val] = (freq[val] || 0) + 1
+    }
+    for(let val in freq) 
+        if(freq[val] > 1){
+            return true
+        }
+        return false
+    }
+
+    console.log(areThereDuplicates4(1, 1, 3, 4))
 
   
