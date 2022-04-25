e3df5f116e6a4482a33627841ce8916f6390a9e9
unknown <sazwerling@gmail.com>
2022-04-25 05:25:13 -0500

    solution for longestSubString

--- longestSubString.js
+++ longestSubString.js
@@ -2,5 +2,19 @@
 //length of the longes substring with all distinct (unique) characters
 
 const longestSubString = function (str) {
-    
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+   
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (seen[char]) {
+        start = Math.max(start, seen[char]);
+      }
+      // index - beginning of substring + 1 (to include current in count)
+      longest = Math.max(longest, i - start + 1);
+      // store the index of the next char so as to not double count
+      seen[char] = i + 1;
+    }
+    return longest;
 }
