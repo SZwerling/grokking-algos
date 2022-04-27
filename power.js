74b4981fe7d9a9019a3b3cda7c4c3c3147872034
unknown <sazwerling@gmail.com>
2022-04-27 11:01:10 -0500

    exponent/base function using recursion

---
+++ power.js
@@ -0,0 +1,12 @@
+//function which takes a base and exponent
+//returns power of base to exponent
+//using recursion
+
+function power(base, exponent){
+    if(exponent === 0){
+        return 1;
+    }
+    return base * (power(base, exponent-1))
+}
+
+console.log(power(2, 4))
