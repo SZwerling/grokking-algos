a530c52b4bcdba1ef708f1dd61288ac9c91df023
unknown <sazwerling@gmail.com>
2022-04-27 09:49:09 -0500

    collect odd numbers from arr w purely recursive function

--- collectOddValues.js
+++ collectOddValues.js
@@ -2,6 +2,7 @@ function collectOdds(arr){
     let result = []
 
     function helper(helperInput){
+
         if(helperInput.length === 0){
             return;
         }
@@ -15,5 +16,20 @@ function collectOdds(arr){
 }
 
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
 console.log(collectOdds(nums))
+
+//pure recursive function // withouth helper method
+
+function pureCollectOdds(arr){
+    let result = []
+    if(arr.length === 0){
+        return result;
+    }
+    if(arr[0] % 2 !==0){
+        result.push(arr[0])
+    }
+    result = result.concat(pureCollectOdds(arr.slice(1)))
+    return result
+}
+
+console.log(pureCollectOdds(nums))
