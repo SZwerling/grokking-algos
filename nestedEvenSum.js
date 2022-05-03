4c78c30668e5f405140969475fb1d0ea7a48b6f2
unknown <sazwerling@gmail.com>
2022-05-03 15:13:58 -0500

    nestedEvenSum solution

--- nestedEvenSum.js
+++ nestedEvenSum.js
@@ -1,6 +1,13 @@
 //recursive function returns sum of all even numbers in an object
 //which may contain nested objects
 
-function nestedEvenSum () {
-    
+function nestedEvenSum (obj, sum=0) {
+    for (var key in obj) {
+        if (typeof obj[key] === 'object'){
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+            sum += obj[key];
+        }
+    }
+    return sum;
 }
