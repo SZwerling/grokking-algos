0f4a44cd5f8799aa82c12428b6d01312b30d6a4c
unknown <sazwerling@gmail.com>
2022-05-12 15:54:56 -0500

    complete naive string search

--- naiveStringSearch.js
+++ naiveStringSearch.js
@@ -7,8 +7,24 @@
 // if complete inner loop and find match, increment count of matches and keep going
 // return count
 
-const naiveStringSeach = function (str1, str2) {
+const naiveStringSeach = function (long, short) {
+   let count = 0;
 
+   for (let i = 0; i < long.length; i++) {
+      for (let j = 0; j < short.length; j++) {
+         if (short[j] !== long[i + j]) {  // this is the clever part
+            break;
+         }
+         
+         if (j === short.length - 1) {
+            count++;
+         }
+      }
+   }
 
-    return  count
-}
+   return count;
+};
+
+const word1 = "ha"; //j
+const word2 = "have a happy"; //i
+console.log(naiveStringSeach(word2, word1));
