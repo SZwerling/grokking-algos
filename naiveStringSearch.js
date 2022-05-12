a4fee536323b82c89e4e38169d0ccbac28ef665a
unknown <sazwerling@gmail.com>
2022-05-12 13:16:31 -0500

    start naive string search

---
+++ naiveStringSearch.js
@@ -0,0 +1,14 @@
+// PSEUDOCODE
+// function takes two strings to see if first string is included in second string
+// loop over the longer string
+// loop over the shorter string
+// if char don't match, break out of inner loop
+// if char's match, keep going
+// if complete inner loop and find match, increment count of matches and keep going
+// return count
+
+const naiveStringSeach = function (str1, str2) {
+
+
+    return  count
+}
