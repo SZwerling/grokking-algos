de1ec37c341286f98f7c000153b8d5a7bb15b29c
unknown <sazwerling@gmail.com>
2022-05-14 16:56:07 -0500

    bubble sort e es6 swap

--- bubbleSort.js
+++ bubbleSort.js
@@ -18,21 +18,6 @@
 const numz = [6, 5, 7, 8, 1, 0];
 // console.log(bubbleSort(numz))
 
-const sort = function (arr) {
-   for (let i = arr.length - 1; i >= 0; i--) {
-      for (let j = 0; j < i; j++) {
-         console.log(`Comparing ${arr[j]} and ${arr[j+1]} and the array is ${arr}`);
-         if (arr[j] > arr[j + 1]) {
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-         }
-      }
-   }
-   return arr
-};
-console.log(sort(numz));
-
 //BUBBLE SORT
 //pseudo code
 //start looping from the end to the beginning with i
@@ -40,17 +25,19 @@ console.log(sort(numz));
 //compare j and j+1 and swap if needed
 //return sorted
 
-// function bubbleSort2(arr){
-//     for(let i = arr.length-1; i >= 0; i--){  //shrink the window on the right every time
-//         for(let j = 0; j < arr.length - i; j++){  //iterate to move largest number to the right
-//              console.log(j)
-//             // if(arr[j] > arr[j+1]){
-//             //     let bigTemp = arr[j]  // the swap, important
-//             //     arr[j] = arr[j+1];
-//             //     arr[j+1] = bigTemp;
-//            // }
-//         }
-//     }
-//     return arr
-// }
-// console.log(bubbleSort2(numz))
+const sort = function (arr) {
+   for (let i = arr.length - 1; i >= 0; i--) {
+      for (let j = 0; j < i; j++) {
+         console.log(`Comparing ${arr[j]} and ${arr[j+1]} and the array is ${arr}`);
+         if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+         }
+      }
+   }
+   return arr
+};
+console.log(sort(numz));
+
+// new 2016 swap
+// [arr[j], arr[j+1] = arr[j+1], arr[j]]
+
