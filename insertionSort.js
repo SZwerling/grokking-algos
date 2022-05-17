a54b276c09cd02764ee0cc7fb6f64ea08deb2d7d
unknown <sazwerling@gmail.com>
2022-05-17 09:51:37 -0500

    start insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,14 @@
+// PSEUDOCODE
+// start w the second element in the arr
+// compare to first one and swap if necessary
+// continue to next element
+// if it is not in correct possition, iterate through sorted portion to place it
+// rinse and repeat
+
+function insertionSort(arr){
+
+}
+
+
+const numz = [2, 1, 9, 76, 4]
+console.log(insertionSort(numz))
