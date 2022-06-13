360f987ff75215a3b5bac5ed84cb4d9080a640a6
unknown <sazwerling@gmail.com>
2022-06-12 21:36:14 -0500

    start doubly linked list class

---
+++ doublyLinkedList.js
@@ -0,0 +1,19 @@
+// Node class has value, next and previous
+
+// Doubly linked list class has head, tail and length
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+}
