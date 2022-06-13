92e6d34ccd0c7c91fcbe99ad6b2b663097f67583
unknown <sazwerling@gmail.com>
2022-06-13 09:10:51 -0500

    push method on doubly linked list

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -16,4 +16,29 @@ class DoublyLinkedList{
         this.head = null;
         this.tail = null;
     }
+    push(val){
+        const newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+
+// PUSH PSEUDOCODE
+// Create new node w value passed to function.
+// If no node, set head and tail property to be newly created node.
+// Otherwise, set next property on tail to be new node and previous
+// property on new node/new tail to be old node. Update tail property to be new node.
+// Increment length and return list.
+
+const list = new DoublyLinkedList
+console.log(list.push('first'))
+console.log(list.push('second'))
+console.log(list.push(3))
