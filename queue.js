2e6fcbaa428f963d8a7efb6a55f30fbdc37546da
unknown <sazwerling@gmail.com>
2022-07-04 14:27:23 -0500

    Queue class from scratch

--- queue.js
+++ queue.js
@@ -9,51 +9,100 @@
 //queue is like a stack, but first added first removed
 //stack is first added last removed
 
-class Queue{
-    constructor(color = ''){
-        this.color = color;
-        this.data = [];
+// class Queue{
+//     constructor(color = ''){  // this constructor is just to make a constructor
+//         this.color = color;
+//         this.data = [];
+//     }
+//     add(record){
+//         this.data.unshift(record);
+//     }
+//     remove(){
+//         return this.data.pop()
+//     }
+//     peek(){
+//         return this.data[this.data.length-1]
+//     }
+// }
+
+// const q1 = new Queue('orange')
+// const q2 = new Queue();
+// q1.add(1)
+// q2.add('1')
+// q1.add(2)
+// q2.add('too')
+// q1.add(3)
+// q2.add('three')
+// q2.add('four')
+// q2.add('five')
+
+
+
+// function weave(x, y){
+//     const len = x.data.length > y.data.length ? x.data.length : y.data.length
+    
+//     const tap = new Queue();
+//     for(let i = 0; i <= len -1; i++){ // or while(x.peek() || y.peek())
+//       if(x.peek()){
+//           tap.add(x.remove())
+//         }
+//       if(y.peek()){
+//           tap.add(y.remove())
+//         }
+//     }
+//         return tap
+// }
+
+
+// console.log(q1)
+
+// QUEUE FROM SCRATCH
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
     }
-    add(record){
-        this.data.unshift(record);
+}
+
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    add(val){
+        const newNode = new Node(val)
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
     }
     remove(){
-        return this.data.pop()
-    }
-    peek(){
-        return this.data[this.data.length-1]
+        if(!this.first) return null;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        let temp = this.first;
+        this.first = this.first.next
+        this.size--;
+        return temp;
+
     }
 }
 
-const q1 = new Queue('orange')
-const q2 = new Queue();
-q1.add(1)
-q2.add('1')
-q1.add(2)
-q2.add('too')
-q1.add(3)
-q2.add('three')
-q2.add('four')
-q2.add('five')
-
-
-
-function weave(x, y){
-    const len = x.data.length > y.data.length ? x.data.length : y.data.length
-    
-    const tap = new Queue();
-    for(let i = 0; i <= len -1; i++){ // or while(x.peek() || y.peek())
-      if(x.peek()){
-          tap.add(x.remove())
-        }
-      if(y.peek()){
-          tap.add(y.remove())
-        }
-    }
-        return tap
-}
-
-
-console.log(q1)
+const cue = new Queue
+cue.add(1)
+cue.add(2)
+console.log(cue.add(3))
+console.log(cue.remove())
+console.log(cue.remove())
+console.log(cue.remove())
+console.log(cue.remove())
+console.log(cue)
 
 
