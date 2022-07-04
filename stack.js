689af6a4a306e2aa26080ac6d1c38ee8ec68495d
unknown <sazwerling@gmail.com>
2022-07-03 19:52:19 -0500

    stack from scratch with push()

--- stack.js
+++ stack.js
@@ -4,28 +4,71 @@
 //stack is like a queue but first added last removed
 //stack is first added last removed
 
+//  IMPLEMENTATION USING ARRAY
+// class Stack{
+//     constructor(){
+//         this.data = [];
+//     }
+//     push(record){
+//         this.data[this.data.length] = record; //could also just use .push()
+//     }
+//     pop(){
+//         return this.data.pop()
+//     }
+//     peek(){
+//         return this.data[this.data.length-1]
+//     }
+// }
 
-class Stack{
-    constructor(){
-        this.data = [];
-    }
-    push(record){
-        this.data[this.data.length] = record; //could also just use .push()
-    }
-    pop(){
-        return this.data.pop()
-    }
-    peek(){
-        return this.data[this.data.length-1]
+// const list = new Stack
+// list.push(1)
+// list.push(2)
+// list.push(3)
+// console.log(list.peek())
+// console.log(list)
+
+// IMPLEMENATION USING LIST-LIKE
+
+class Node {
+    constructor(value){
+        this.val = value;
+        this.next = null;
     }
 }
 
-const list = new Stack
-list.push(1)
-list.push(2)
-list.push(3)
-console.log(list.peek())
-console.log(list)
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val){
+        const newNode = new Node(val);
+        if(this.size === 0){
+            this.first = newNode;
+            this.last = newNode;
+        
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        this.size++;
+        return this.size;
+    }
+}
+
+const stack = new Stack()
+console.log(stack.push(1))
+console.log(stack.push(2))
+console.log(stack)
+
+// PUSH PSEUDOCODE
+// Function accepts value and creates new node.
+// If no other values, set first and last to new node.
+// Otherwise make temp, put current first in there, than make
+// new first the first with its next as the old first.
+
 
 
 
